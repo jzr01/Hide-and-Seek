8aabf164de845ce5b2f6e016454335c5bee272a7
Zhengran Ji <70544438+jzr01@users.noreply.github.com>
2023-09-07 08:59:44 -0400

    Add files via upload

---
+++ Assets/HideScipt.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideScipt : MonoBehaviour
+{
+ 
+    public float normalspeed;
+    public float sprintspeed;
+    private float speed;
+
+    public float stmina = 3.0f;
+    public float relaxtime = 4.0f;
+
+    private float timesincesprint = 0.0f;
+    private float timesincerelax = 0.0f;
+
+    private Collider[] hitcolliders;
+    private RaycastHit Hit;
+
+    public float detectrange;
+
+    public Rigidbody rb;
+    public GameObject target;
+
+    private bool seeseeker;
+
+    public float directionChangeInterval = 2f; // Time in seconds before changing direction
+
+    private float timeSinceLastDirectionChange = 0.0f;
+    private Vector3 currentDirection = Vector3.forward.normalized;
+
+    private Vector3 lastKnownHiderPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = normalspeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // detect if there a hider in range 
+        if (!seeseeker)
+        {
+            speed = normalspeed;
+            // Move the object in the current direction
+            transform.Translate(currentDirection * speed * Time.deltaTime);
+
+            // Update the timer
+            timeSinceLastDirectionChange += Time.deltaTime;
+
+            // Check if it's time to change direction
+            if (timeSinceLastDirectionChange >= directionChangeInterval)
+            {
+                // Change to a new random direction
+                currentDirection = Random.insideUnitSphere.normalized;
+
+                // Reset the timer
+                timeSinceLastDirectionChange = 0.0f;
+            }
+
+            hitcolliders = Physics.OverlapSphere(transform.position, detectrange);
+            foreach (var hitcollider in hitcolliders)
+            {
+                if (hitcollider.tag == "Seeker")
+                {
+                    target = hitcollider.gameObject;
+                    seeseeker = true;
+                    lastKnownHiderPosition = target.transform.position;
+                }
+            }
+        }
+        else
+        {
+
+            if (timesincesprint <= stmina)
+            {
+                speed = sprintspeed;
+                timesincesprint += Time.deltaTime;
+            }
+            else
+            {
+                if (timesincerelax < relaxtime)
+                {
+                    speed = normalspeed;
+                    timesincerelax += Time.deltaTime;
+
+                }
+                else
+                {
+                    speed = sprintspeed;
+                    timesincesprint = 0.0f;
+                    timesincerelax = 0.0f;
+                }
+
+            }
+
+
+            var Heading = target.transform.position - transform.position;
+            var Distance = Heading.magnitude;
+            var Direction = Heading / Distance;
+
+
+            Vector3 Move = new Vector3(-Direction.x * speed, 0, -Direction.z * speed);
+            rb.velocity = Move;
+            transform.forward = Move;
+
+            seeseeker = false;
+
+            hitcolliders = Physics.OverlapSphere(transform.position, detectrange);
+            foreach (var hitcollider in hitcolliders)
+            {
+                if (hitcollider.tag == "Seeker")
+                {
+                    target = hitcollider.gameObject;
+                    seeseeker = true;
+                }
+            }
+        }
+
+    }
+
+    // Collision detection with the Hider
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Seeker"))
+        {
+            normalspeed = 0;
+            sprintspeed = 0;
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Calculate the reflection vector off the wall
+            Vector3 normal = collision.contacts[0].normal;
+            currentDirection = Vector3.Reflect(currentDirection, normal);
+        }
+    }
+
+}
+
